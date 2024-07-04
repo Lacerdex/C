65ec410fc7b6ceefb556b79581c4af2ea63ad6d1
Gabriel Lacerda <162513974+Lacerdex@users.noreply.github.com>
2024-07-04 12:58:22 -0300

    forca.h
    Um header file do "forca.c" feito em C.

---
+++ forca.h
@@ -0,0 +1,11 @@
+#define TAMANHO_PALAVRA 20
+
+void abertura();
+void chuta();
+void desenhaforca();
+void escolhepalavra();
+int chuteserrados();
+int jachutou(char letra);
+int enforcou();
+int acertou();
+void adicionapalavra();
